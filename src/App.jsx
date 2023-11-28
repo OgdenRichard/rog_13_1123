f28e04e8f6b01f1747f81b7e15b1fe33d97873b8
Richard Ogden <ogden.richard@gmail.com>
2023-11-28 16:38:35 +0100

    28112023 | base router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
-import { CakeView } from './features/cake/CakeView';
+import {
+  createBrowserRouter,
+  Route,
+  createRoutesFromElements,
+  RouterProvider,
+} from 'react-router-dom';
+import { Root } from './layouts/Root';
 import './App.css';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(<Route path="/" element={<Root />} />),
+);
 
 function App() {
   return (
     <>
       <div className="App">
-        <CakeView />
+        <RouterProvider router={router} />
       </div>
     </>
   );
---
+++ src/layouts/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <>
+      <header>bonjour</header>
+    </>
+  );
+}
+
+export default Header;
