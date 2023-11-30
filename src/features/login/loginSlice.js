f42b21afcc2e620d7d481a1ceefd367cf92e008c
Richard Ogden <ogden.richard@gmail.com>
2023-11-30 14:37:06 +0100

    30112023 | saync thunk setup

--- src/features/login/loginSlice.js
+++ src/features/login/loginSlice.js
@@ -1,11 +1,27 @@
-import { createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import API_BASE_URL from '../../config/apiSettings';
 
 const initialState = {
   isLoggedIn: false,
   login: '',
   password: '',
   token: '',
+  loading: false,
+  data: [],
+  error: '',
 };
+
+export const userLogin = createAsyncThunk(
+  'login/userLogin',
+  async (loginData) => {
+    const body = JSON.stringify(loginData);
+    const response = await axios.post(`${API_BASE_URL}/user/login`, body, {
+      headers: { 'Content-Type': 'application/json' },
+    });
+    return response.data;
+  },
+);
 
 const loginSlice = createSlice({
   name: 'login',
@@ -17,6 +33,21 @@ const loginSlice = createSlice({
     setPassword: (state, action) => {
       state.password = action.payload;
     },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(userLogin.pending, (state) => {
+      state.loading = true;
+    });
+    builder.addCase(userLogin.fulfilled, (state, action) => {
+      state.loading = false;
+      state.data = action.payload;
+      state.error = '';
+    });
+    builder.addCase(userLogin.rejected, (state, action) => {
+      state.loading = false;
+      state.data = [];
+      state.error = action.error.message;
+    });
   },
 });
 
