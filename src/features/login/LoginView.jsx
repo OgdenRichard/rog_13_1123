691c119894b6e5c804a3d9ccebd7d55d7648b403
Richard Ogden <ogden.richard@gmail.com>
2023-12-04 11:17:11 +0100

    04122023 | navigate to profile on login + icons switch

--- src/features/login/LoginView.jsx
+++ src/features/login/LoginView.jsx
@@ -1,48 +1,55 @@
 import { useSelector, useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import { setPassword, setUsername, setRemember, userLogin } from './loginSlice';
 
 function LoginView() {
   const username = useSelector((state) => state.login.login);
   const pass = useSelector((state) => state.login.password);
+  const userConnected = useSelector((state) => state.login.isLoggedIn);
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   return (
     <>
-      <form action="">
-        <div className="input-wrapper">
-          <label htmlFor="username">Username</label>
-          <input
-            type="text"
-            id="username"
-            onChange={(e) => dispatch(setUsername(e.target.value))}
-          />
-        </div>
-        <div className="input-wrapper">
-          <label htmlFor="password">Password</label>
-          <input
-            type="text"
-            id="password"
-            onChange={(e) => dispatch(setPassword(e.target.value))}
-          />
-        </div>
-        <div className="input-remember">
-          <label htmlFor="remember-me">Remember me</label>
-          <input
-            type="checkbox"
-            id="remember-me"
-            onChange={(e) => dispatch(setRemember(e.target.checked))}
-          />
-        </div>
-        <button
-          type="button"
-          className="sign-in-button"
-          onClick={() =>
-            dispatch(userLogin({ email: username, password: pass }))
-          }
-        >
-          Sign In
-        </button>
-      </form>
+      {!userConnected ? (
+        <form action="">
+          <div className="input-wrapper">
+            <label htmlFor="username">Username</label>
+            <input
+              type="text"
+              id="username"
+              onChange={(e) => dispatch(setUsername(e.target.value))}
+            />
+          </div>
+          <div className="input-wrapper">
+            <label htmlFor="password">Password</label>
+            <input
+              type="text"
+              id="password"
+              onChange={(e) => dispatch(setPassword(e.target.value))}
+            />
+          </div>
+          <div className="input-remember">
+            <label htmlFor="remember-me">Remember me</label>
+            <input
+              type="checkbox"
+              id="remember-me"
+              onChange={(e) => dispatch(setRemember(e.target.checked))}
+            />
+          </div>
+          <button
+            type="button"
+            className="sign-in-button"
+            onClick={() =>
+              dispatch(userLogin({ email: username, password: pass }))
+            }
+          >
+            Sign In
+          </button>
+        </form>
+      ) : (
+        navigate('/profile')
+      )}
     </>
   );
 }
--- src/layouts/Navbar.jsx
+++ src/layouts/Navbar.jsx
@@ -1,7 +1,9 @@
 import { NavLink } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import logo from '../assets/argentBankLogo.png';
 
 export const Navbar = () => {
+  const userConnected = useSelector((state) => state.login.isLoggedIn);
   return (
     <nav className="main-nav">
       <NavLink to="/home" style={{ textDecoration: 'none' }}>
@@ -14,7 +16,10 @@ export const Navbar = () => {
       <NavLink to="/profile">Test</NavLink>
       <NavLink to="/signin" style={{ textDecoration: 'none' }}>
         <div className="main-nav-item">
-          <i className="fa fa-user-circle" /> <span>Sign In</span>
+          <i className="fa fa-user-circle" />
+          {userConnected && <span className="nav-username">Jean-Michel</span>}
+          {userConnected && <i className="fa fa-sign-out" />}
+          <span>{userConnected ? 'Sign Out' : 'Sign In'}</span>
         </div>
       </NavLink>
     </nav>
