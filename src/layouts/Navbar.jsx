69629dff331b78ae3136eb30a1b93398d870e577
Richard Ogden <ogden.richard@gmail.com>
2023-12-04 15:56:56 +0100

    04122023 | switch navlink log in/out

--- src/layouts/Navbar.jsx
+++ src/layouts/Navbar.jsx
@@ -13,15 +13,34 @@ export const Navbar = () => {
           alt="Argent Bank Logo"
         />
       </NavLink>
-      <NavLink to="/profile">Test</NavLink>
-      <NavLink to="/signin" style={{ textDecoration: 'none' }}>
-        <div className="main-nav-item">
-          <i className="fa fa-user-circle" />
-          {userConnected && <span className="nav-username">Jean-Michel</span>}
-          {userConnected && <i className="fa fa-sign-out" />}
-          <span>{userConnected ? 'Sign Out' : 'Sign In'}</span>
-        </div>
-      </NavLink>
+      <div className="main-nav-item">
+        <span className="nav-username">
+          <i className="fa fa-user-circle icon-username" />
+          {userConnected && (
+            <NavLink
+              to="/profile"
+              style={{ textDecoration: 'none', marginRight: '0.5em' }}
+            >
+              Jean-Michel
+            </NavLink>
+          )}
+        </span>
+        <span>
+          {userConnected && (
+            <>
+              <i className="fa fa-sign-out" />
+              <NavLink to="/home" style={{ textDecoration: 'none' }}>
+                Sign Out
+              </NavLink>
+            </>
+          )}
+          {!userConnected && (
+            <NavLink to="/signin" style={{ textDecoration: 'none' }}>
+              Sign In
+            </NavLink>
+          )}
+        </span>
+      </div>
     </nav>
   );
 };
