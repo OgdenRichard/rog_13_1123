7ee1b26cbbd50d8988912bf085dd3238a631bc98
Richard Ogden <ogden.richard@gmail.com>
2023-12-06 14:44:26 +0100

    06122023 | ProfileView > display username

--- src/features/profile/ProfileView.jsx
+++ src/features/profile/ProfileView.jsx
@@ -5,22 +5,33 @@ import { getUserData } from './profileSlice';
 function ProfileView() {
   const userConnected = useSelector((state) => state.login.isLoggedIn);
   const token = useSelector((state) => state.login.auth.token);
+  const loading = useSelector((state) => state.profile.status.loading);
+  const data = useSelector((state) => state.profile.data);
   const dispatch = useDispatch();
+
   useEffect(() => {
     if (userConnected) {
       dispatch(getUserData(token));
     }
   }, [userConnected, dispatch, token]);
+
   return (
     <>
-      <div className="user-header">
-        <h1>
-          Welcome back <br /> Jean-Michel
-        </h1>
-        <button type="button" className="edit-button">
-          Edit name
-        </button>
-      </div>
+      {!loading && data ? (
+        <>
+          <div className="user-header">
+            <h1>
+              Welcome back <br />
+              {data.firstName || ''} {data.lastName || ''}
+            </h1>
+            <button type="button" className="edit-button">
+              Edit name
+            </button>
+          </div>
+        </>
+      ) : (
+        <>Loading</>
+      )}
     </>
   );
 }
