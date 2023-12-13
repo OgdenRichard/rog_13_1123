d4ae8d79e94e7db2400d192108a063e3e0d91179
Richard Ogden <ogden.richard@gmail.com>
2023-12-13 15:36:59 +0100

    13122023 | redux-persist + thunk + implementation

--- src/app/store.js
+++ src/app/store.js
@@ -1,14 +1,28 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import storage from 'redux-persist/lib/storage';
+import { persistReducer, persistStore } from 'redux-persist';
+import thunk from 'redux-thunk';
 import loginReducer from '../features/login/loginSlice';
 import profileReducer from '../features/profile/profileSlice';
 import modalReducer from '../features/modal/modalSlice';
 
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const rootReducer = combineReducers({
+  login: loginReducer,
+  profile: profileReducer,
+  edit: modalReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 const store = configureStore({
-  reducer: {
-    login: loginReducer,
-    profile: profileReducer,
-    edit: modalReducer,
-  },
+  reducer: persistedReducer,
+  middleware: [thunk],
 });
 
 export default store;
+export const persistor = persistStore(store);
--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 import App from './app/App';
-import store from './app/store';
+import store, { persistor } from './app/store';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
 );
